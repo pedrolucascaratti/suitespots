88ce8a7491cdcd10b6602ba757ba57c7a484f6d1
Pedro Lucas Caratti <pedrolucascaratti@gmail.com>
2023-05-27 17:15:49 -0400

    Primeiro commit

---
+++ src/main/java/prog/orient/objeto/suitespots/Suitespots.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package prog.orient.objeto.suitespots;
+
+/**
+ *
+ * @author pedro
+ */
+public class Suitespots {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
